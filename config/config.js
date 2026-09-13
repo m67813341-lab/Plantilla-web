b0cd4444b9bb3fdbeac7dc5bb05ca00e7161a3b6
m67813341-lab <m67813341@gmail.com>
2026-09-13 09:47:30 -0300

    Update principal image URL with query parameter

--- config/config.js
+++ config/config.js
@@ -24,7 +24,7 @@ const CONFIG = {
 
     imagenes: {
 
-        principal: "img/IMG-20260503-WA0083.jpg",
+        principal: "img/IMG-20260503-WA0083.jpg?prueba=1",
 
         nosotros: "img/IMG-20260503-WA0083.jpg",
 
